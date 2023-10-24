ed969af13fad1c99293e3441877ee7cb24b12b14
Lucas Amaral <lucasamaralfaria@gmail.com>
2023-10-23 21:18:43 -0300

    final

--- implementacoes/melhor_primeiro.py
+++ implementacoes/melhor_primeiro.py
@@ -45,38 +45,7 @@ def best_first_search(root):
                 o_brabo = vizinho
         
         state = o_brabo
-
-        
-
-
-
-
-    # while not open.empty():
-    #     v_heuristica,next_state = open.get()
-    #     print(next_state)
-    #     aux = ultimo
-    #     ultimo = next_state
-        
-    #     if v_heuristica == 0:
-    #         break
-
-
-    #     nos_viznhos = vizinhos(next_state)
-
-    #     for no in nos_viznhos:
-    #         if no not in close:
-    #             open.put((heuristic(no),no))
-        
-
-    #     close.append(next_state)
-
     
-    
-
-        # print(state)
-
-        
-
 
     return state
 
@@ -139,20 +108,8 @@ def heuristic(state):
 
     return diferenca
 
-# def heuristic(state):
-#     matriz = [[1,2,3],[4,5,6],[7,8,0]]
-#     diferenca = 0
-
-#     for i in range(len(matriz)):
-#         for j in range(len(matriz[0])):
-#             diferenca += abs(matriz[i][j] - state[i][j])
-
-#     return diferenca
-
 
 posicoes_corretas = [(2,2),(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1)]
-
-
 
 
 matriz = [[1,2,3],[7,8,0],[4,5,6]]
